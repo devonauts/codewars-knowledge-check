38761c29ee42f44ecab2bd736dc33066ada81ec5
Devonauts <56298990+devonauts@users.noreply.github.com>
2025-06-02 11:05:08 -0500

    first commit

---
+++ CodewarAnswers.py
@@ -0,0 +1,16 @@
+#solution 1 Even or odd:
+def even_or_odd(number):
+    
+    if number%2 == 0:
+        return ("Even")
+    else:
+        return ("Odd")
+    pass
+
+#solution 2 Convert a Number to a String
+def number_to_string(num):
+     return f"{num}"
+ 
+ #solution 3 Remove String Spaces
+def no_space(x):
+    return x.replace(" ", "")
